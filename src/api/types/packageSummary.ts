2fbcf8ad157526c699be4529deaa70d4054b9fb6
Oleksandr Pavlii <pavliy.av@gmail.com>
2025-04-06 18:55:28 +0200

    implement search results

--- src/api/types/packageSummary.ts
+++ src/api/types/packageSummary.ts
@@ -1,6 +1,6 @@
 export interface PackageSummary {
-    name: string;
-    version: string;
-    description: string;
-    keywords: string[];
+	name: string;
+	version: string;
+	description: string;
+	keywords?: string[];
 }
