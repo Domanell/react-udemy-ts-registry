3ed9aade68318a190796cad63ca833fc1178dcce
Oleksandr Pavlii <pavliy.av@gmail.com>
2025-04-06 19:58:34 +0200

    Add package details

---
+++ src/pages/details/detailsLoader.ts
@@ -0,0 +1,24 @@
+import type { Params } from 'react-router-dom';
+import type { PackageDetails } from '../../api/types/packageDetails';
+import getPackage from '../../api/queries/getPackage';
+
+interface LoaderArgs {
+	params: Params;
+}
+
+export interface DetailsLoaderResult {
+	details: PackageDetails;
+}
+
+export default async function detailsLoader({ params }: LoaderArgs): Promise<DetailsLoaderResult> {
+	const { name } = params;
+	if (!name) {
+		throw new Error('Name must be provided');
+	}
+
+	const details = await getPackage(name);
+
+	return {
+		details,
+	};
+}
